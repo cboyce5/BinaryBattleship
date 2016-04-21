161eca60427b74f73ba7793531c531ba96aed37e
Edgar Gutierrez <egutierr@mymail.mines.edu>
2016-04-21 16:26:13 -0600

    Cleaned up unnecessary testing and class

--- src/BattleshipGame/ThreeBitBadBitException.java
+++
@@ -1,9 +0,0 @@
-package BattleshipGame;
-
-@SuppressWarnings("serial")
-public class ThreeBitBadBitException extends Exception {
-	
-	public ThreeBitBadBitException() {
-		super("Invalid bit detected; a bit can only be 0 or 1");
-	}
-}
